20e86f3bdb93f4867b5aa8268c1ea8f0d7c5073e
dev <elena.avdiunina@gmail.com>
2022-01-05 11:50:26 +0200

    Updates

---
+++ precache-manifest.3ca0ac93e46ced077bc5bcce9f95ca92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3498a7433017d3daeac3fecef955f5b3",
+    "url": "/burgers-app/index.html"
+  },
+  {
+    "revision": "4461cc5e027cca04b5ff",
+    "url": "/burgers-app/static/css/main.b8ae5d13.chunk.css"
+  },
+  {
+    "revision": "db83edcf914cc8a95c44",
+    "url": "/burgers-app/static/js/2.5e272f95.chunk.js"
+  },
+  {
+    "revision": "4461cc5e027cca04b5ff",
+    "url": "/burgers-app/static/js/main.1674799f.chunk.js"
+  },
+  {
+    "revision": "b7b55c37a5ac7c86c301",
+    "url": "/burgers-app/static/js/runtime-main.aad7824e.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/burgers-app/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/burgers-app/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/burgers-app/static/media/up.551b5c6b.svg"
+  }
+]);
